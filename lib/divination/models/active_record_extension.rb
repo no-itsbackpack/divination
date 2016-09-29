09a202260aed73a8573f322650aa63db5ced2e3c
Elom Gomez <gomezelom@yahoo.com>
2016-09-28 22:10:55 -0500

    Fix depracated alias_method_chain

--- lib/divination/models/active_record_extension.rb
+++ lib/divination/models/active_record_extension.rb
@@ -3,16 +3,16 @@ require 'divination/models/active_record_model_extension'
 module Divination
   module ActiveRecordExtension
     extend ActiveSupport::Concern
-    included do
-      # Future subclasses will pick up the model extension
-      class << self
-        def inherited_with_divination(kls) #:nodoc:
-          inherited_without_divination kls
-          kls.send(:include, Divination::ActiveRecordModelExtension) if kls.superclass == ::ActiveRecord::Base
-        end
-        alias_method_chain :inherited, :divination
-      end
 
+    module ClassMethods
+      # Future subclasses will pick up the model extension
+      def inherited(kls) #:nodoc:
+        super
+        kls.send(:include, Divination::ActiveRecordModelExtension) if kls.superclass == ::ActiveRecord::Base
+      end
+    end
+
+    included do
       # Existing subclasses pick up the model extension as well
       self.descendants.each do |kls|
         kls.send(:include, Divination::ActiveRecordModelExtension) if kls.superclass == ::ActiveRecord::Base
--- lib/divination/version.rb
+++ lib/divination/version.rb
@@ -1,3 +1,3 @@
 module Divination
-  VERSION = "0.2.0"
+  VERSION = "0.2.1"
 end
